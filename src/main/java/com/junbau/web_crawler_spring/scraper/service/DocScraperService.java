16b59f210b91a4b795e63d78fb55e74722be3900
Phillip Alejo <phillip.alejo@outlook.com>
2025-04-20 22:41:38 +0100

    Fix unit test
    Signed-off-by: Phillip Alejo <phillip.alejo@outlook.com>

--- src/main/java/com/junbau/web_crawler_spring/scraper/service/DocScraperService.java
+++ src/main/java/com/junbau/web_crawler_spring/scraper/service/DocScraperService.java
@@ -7,11 +7,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
@@ -51,14 +48,5 @@ public class DocScraperService implements  ScraperService {
 
         PutObjectResponse response = s3Client.putObject(putObjectRequest, scrapedHtml);
         logger.info(response.toString());
-
-        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                .bucket(SCRAPED_PAGE_BUCKET)
-                .key(fileName)
-                .build();
-
-        // Retrieving the object from the bucket.
-        ResponseInputStream<GetObjectResponse> objResponse = s3Client.getObject(getObjectRequest);
-        logger.info(objResponse.toString());
     }
 }
--- src/test/java/com/junbau/web_crawler_spring/scraper/DocScraperServiceTest.java
+++ src/test/java/com/junbau/web_crawler_spring/scraper/DocScraperServiceTest.java
@@ -1,19 +1,41 @@
 package com.junbau.web_crawler_spring.scraper;
 
+import com.junbau.web_crawler_spring.aws.s3.AwsS3ClientBuilderService;
 import com.junbau.web_crawler_spring.scraper.service.DocScraperService;
 import com.junbau.web_crawler_spring.scraper.service.ScraperService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.commons.util.StringUtils;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class DocScraperServiceTest {
 
+    @Mock
+    AwsS3ClientBuilderService awsS3ClientBuilderService;
+    @Mock
+    S3Client s3Client;
+
+    @InjectMocks
     ScraperService scraperService = new DocScraperService();
 
     @Test
     void shouldReturnDocumentWithValidURL() {
         String url = "https://www.scrapingcourse.com/ecommerce/";
+        when(s3Client.putObject(any(PutObjectRequest.class), any(RequestBody.class))).thenReturn(mock(PutObjectResponse.class));
+        when(awsS3ClientBuilderService.createAwsS3Client("test", "test", "eu-west-1")).thenReturn(s3Client);
         String document = scraperService.getDocument(url);
         assertTrue(StringUtils.isNotBlank(document));
     }
